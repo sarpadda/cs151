6ca433085483d1390f3b9ca213749cc7123e5d66
Sar Padda <sarpadda@Sars-MacBook-Pro.local>
2019-03-12 20:43:19 -0700

    hw3

--- hw3/Question2 hw cs151/src/TimerTest.java
+++ hw3/Question2 hw cs151/src/TimerTest.java
@@ -5,14 +5,14 @@ import javax.swing.*;
 
 public class TimerTest {
     public static void main(String [] args){
-    	
-        Timer timer = new Timer(1000, new ActionListener(){
+    	 JFrame frame = new JFrame();
+    		int delay=1000;
+        Timer timer = new Timer(delay, new ActionListener(){
             public void actionPerformed(ActionEvent event) {
             	System.out.println("Hello world!");
             }
         });
-        while(true) {
-        	timer.start();	
-        }
+        
+         	timer.start();           
     }
 }
